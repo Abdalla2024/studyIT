ef4a921cd60a5d05dcd7410044bbbabc494e1330
Abdalla2024 <abdallaa2024@gmail.com>
2025-06-11 19:52:04 -0400

    Starting xcode project

---
+++ security/security/securityApp.swift
@@ -0,0 +1,17 @@
+//
+//  securityApp.swift
+//  security
+//
+//  Created by Abdalla Abdelmagid on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct securityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ security/securityTests/securityTests.swift
@@ -0,0 +1,17 @@
+//
+//  securityTests.swift
+//  securityTests
+//
+//  Created by Abdalla Abdelmagid on 6/11/25.
+//
+
+import Testing
+@testable import security
+
+struct securityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
