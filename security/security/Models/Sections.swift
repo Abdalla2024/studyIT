3e397691d77b0fd8fd1f9d419040a96670297a69
Abdalla2024 <abdallaa2024@gmail.com>
2025-06-12 23:48:50 -0400

    Created model for sections

--- security/security/Models/Sections.swift
+++ security/security/Models/Sections.swift
@@ -5,3 +5,24 @@
 //  Created by Abdalla Abdelmagid on 6/12/25.
 //
 
+import Foundation
+import MongoDB
+
+struct Section: Codable, Identifiable {
+    var _id: ObjectId
+    var title: String
+    var displayOrder: String  // For example: "1.1", "2.3", etc.
+    var score: Double  // Percentage score
+    
+    var id: String {
+        return _id.hexString
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case _id
+        case title
+        case displayOrder
+        case score
+    }
+}
+
